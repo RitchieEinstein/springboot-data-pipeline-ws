e85b0d07f425aea84db0ed62b8d22cec8b14d474
Ritchie Einstein <ritchie.einstein@gmail.com>
2020-10-18 00:10:54 +0530

    WebSocket Streamer Boilerplate code

---
+++ websock-streamer/src/main/java/com/ritchieeinstein/falcon/websockstreamer/WebsockStreamerApplication.java
@@ -0,0 +1,13 @@
+package com.ritchieeinstein.falcon.websockstreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockStreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsockStreamerApplication.class, args);
+	}
+
+}
---
+++ websock-streamer/src/test/java/com/ritchieeinstein/falcon/websockstreamer/WebsockStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritchieeinstein.falcon.websockstreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsockStreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
