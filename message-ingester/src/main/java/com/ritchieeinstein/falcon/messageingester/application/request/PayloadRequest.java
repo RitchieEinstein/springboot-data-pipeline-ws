4dbcccf28e9a0cc71e6e8d18729b03a4f9cdb275
Ritchie Einstein <ritchie.einstein@gmail.com>
2020-10-17 22:40:14 +0530

    TimestampConstraint Enabled and Payload To Message Model mapped

--- message-ingester/src/main/java/com/ritchieeinstein/falcon/messageingester/application/request/PayloadRequest.java
+++ message-ingester/src/main/java/com/ritchieeinstein/falcon/messageingester/application/request/PayloadRequest.java
@@ -1,12 +1,19 @@
 package com.ritchieeinstein.falcon.messageingester.application.request;
 
+import com.ritchieeinstein.falcon.messageingester.application.validators.TimestampConstraint;
+import com.ritchieeinstein.falcon.messageingester.domain.model.MessagePayload;
+
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.beans.Transient;
 
 public class PayloadRequest {
 
     @NotEmpty
     private String content;
 
+    @TimestampConstraint
     private String timestamp;
 
     public PayloadRequest(String content, String timestamp) {
@@ -28,5 +35,10 @@ public class PayloadRequest {
 
     public void setTimestamp(String timestamp) {
         this.timestamp = timestamp;
+    }
+
+    @Transient
+    public MessagePayload getMessagePayload(){
+        return new MessagePayload(content, timestamp);
     }
 }
