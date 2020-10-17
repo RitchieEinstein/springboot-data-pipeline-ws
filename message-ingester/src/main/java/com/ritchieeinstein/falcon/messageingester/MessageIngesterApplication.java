502c58d6cb81b4f16dc5e3d250ec1dd95b7ec603
Ritchie Einstein <ritchie.einstein@gmail.com>
2020-10-17 21:28:06 +0530

    Spring Initializer created Project with Web, DevTools and Cloud Stream dependencies

---
+++ message-ingester/src/main/java/com/ritchieeinstein/falcon/messageingester/MessageIngesterApplication.java
@@ -0,0 +1,13 @@
+package com.ritchieeinstein.falcon.messageingester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageIngesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageIngesterApplication.class, args);
+	}
+
+}
