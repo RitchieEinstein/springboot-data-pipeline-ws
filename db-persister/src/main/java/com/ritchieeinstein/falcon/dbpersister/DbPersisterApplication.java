a16f0511f385b083cd5bffa66be3bcb60d72f0ea
Ritchie Einstein <ritchie.einstein@gmail.com>
2020-10-18 02:26:33 +0530

    Spring Boot Boiler Plate Code Created

---
+++ db-persister/src/main/java/com/ritchieeinstein/falcon/dbpersister/DbPersisterApplication.java
@@ -0,0 +1,13 @@
+package com.ritchieeinstein.falcon.dbpersister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbPersisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbPersisterApplication.class, args);
+	}
+
+}
