9b0933895edbe08ecba881f1e977de7c484915d4
Ritchie Einstein <ritchie.einstein@gmail.com>
2020-10-17 23:52:47 +0530

    Controller is now wired to communicate with service to delegate the task

--- message-ingester/src/main/java/com/ritchieeinstein/falcon/messageingester/application/rest/controller/PayloadController.java
+++ message-ingester/src/main/java/com/ritchieeinstein/falcon/messageingester/application/rest/controller/PayloadController.java
@@ -2,8 +2,10 @@ package com.ritchieeinstein.falcon.messageingester.application.rest.controller;
 
 import com.ritchieeinstein.falcon.messageingester.application.request.PayloadRequest;
 import com.ritchieeinstein.falcon.messageingester.application.response.GenericResponse;
+import com.ritchieeinstein.falcon.messageingester.domain.service.MessagePayloadService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -19,9 +21,13 @@ public class PayloadController {
 
     private Logger LOGGER = LoggerFactory.getLogger(PayloadController.class);
 
+    @Autowired
+    private MessagePayloadService messagePayloadService;
+
     @PostMapping("/message")
     public ResponseEntity<GenericResponse<PayloadRequest>> ingestMessagePayload(@Valid @RequestBody PayloadRequest payloadRequest){
         LOGGER.debug(payloadRequest.getContent() + " " + payloadRequest.getTimestamp());
+        messagePayloadService.pushMessageIntoQueue(payloadRequest.getMessagePayload());
         return new ResponseEntity<>(new GenericResponse<>(payloadRequest), HttpStatus.ACCEPTED);
     }
 
