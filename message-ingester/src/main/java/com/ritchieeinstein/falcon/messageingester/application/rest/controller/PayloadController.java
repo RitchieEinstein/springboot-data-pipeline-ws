11f46e21ac3aae85aa62cca3fcf2480630114fde
Ritchie Einstein <ritchie.einstein@gmail.com>
2020-10-17 22:40:35 +0530

    Request Responds with Proper HttpStatus

--- message-ingester/src/main/java/com/ritchieeinstein/falcon/messageingester/application/rest/controller/PayloadController.java
+++ message-ingester/src/main/java/com/ritchieeinstein/falcon/messageingester/application/rest/controller/PayloadController.java
@@ -3,6 +3,8 @@ package com.ritchieeinstein.falcon.messageingester.application.rest.controller;
 import com.ritchieeinstein.falcon.messageingester.application.request.PayloadRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,9 +19,9 @@ public class PayloadController {
     private Logger LOGGER = LoggerFactory.getLogger(PayloadController.class);
 
     @PostMapping("/message")
-    public PayloadRequest ingestMessagePayload(@Valid @RequestBody PayloadRequest payloadRequest){
+    public ResponseEntity<PayloadRequest> ingestMessagePayload(@Valid @RequestBody PayloadRequest payloadRequest){
         LOGGER.debug(payloadRequest.getContent() + " " + payloadRequest.getTimestamp());
-        return payloadRequest;
+        return new ResponseEntity<>(payloadRequest, HttpStatus.ACCEPTED);
     }
 
 
