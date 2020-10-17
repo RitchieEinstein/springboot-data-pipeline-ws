5cbc51f0e09555af6ed7748f8ad86f108728e5d4
Ritchie Einstein <ritchie.einstein@gmail.com>
2020-10-17 22:56:11 +0530

    ControllerAdvice Implemented to handle Validation responses

---
+++ message-ingester/src/main/java/com/ritchieeinstein/falcon/messageingester/application/rest/controller/advice/MethodArgumentNotValidExceptionHandler.java
@@ -0,0 +1,65 @@
+package com.ritchieeinstein.falcon.messageingester.application.rest.controller.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class MethodArgumentNotValidExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public Error handleMethodArgumentNotValidException(org.springframework.web.bind.MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+
+        List<String> errorList = new ArrayList<>();
+        result.getFieldErrors().forEach((fieldError) -> errorList.add("'" + fieldError.getField() + "' " + fieldError.getDefaultMessage() +" : rejected value [" +fieldError.getRejectedValue() +"]" ));
+        result.getGlobalErrors().forEach((fieldError) -> errorList.add(fieldError.getObjectName()+" : " +fieldError.getDefaultMessage() ));
+
+        return new Error(HttpStatus.BAD_REQUEST, errorList);
+    }
+
+    public static class Error{
+        private int errorCode;
+        private String error;
+        private List<String> errorFields;
+
+        public Error(HttpStatus status, List<String> errorFields) {
+            this.errorCode = status.value();
+            this.error = status.name();
+            this.errorFields = errorFields;
+        }
+
+        public int getErrorCode() {
+            return errorCode;
+        }
+
+        public void setErrorCode(int errorCode) {
+            this.errorCode = errorCode;
+        }
+
+        public String getError() {
+            return error;
+        }
+
+        public void setError(String error) {
+            this.error = error;
+        }
+
+        public List<String> getErrorFields() {
+            return errorFields;
+        }
+
+        public void setErrorFields(List<String> errorFields) {
+            this.errorFields = errorFields;
+        }
+    }
+}
