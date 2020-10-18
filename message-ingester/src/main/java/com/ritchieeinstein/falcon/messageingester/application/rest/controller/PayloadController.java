4ffb95ad203be24caeeddcfa6b4885a7504c9433
Ritchie Einstein <ritchie.einstein@gmail.com>
2020-10-18 22:20:24 +0530

    GET API Endpoint implemented to handle the enriched data request

--- message-ingester/src/main/java/com/ritchieeinstein/falcon/messageingester/application/rest/controller/PayloadController.java
+++ message-ingester/src/main/java/com/ritchieeinstein/falcon/messageingester/application/rest/controller/PayloadController.java
@@ -1,6 +1,7 @@
 package com.ritchieeinstein.falcon.messageingester.application.rest.controller;
 
 import com.ritchieeinstein.falcon.messageingester.application.request.PayloadRequest;
+import com.ritchieeinstein.falcon.messageingester.application.response.EnrichedMessageDTO;
 import com.ritchieeinstein.falcon.messageingester.application.response.GenericResponse;
 import com.ritchieeinstein.falcon.messageingester.domain.service.MessagePayloadService;
 import org.slf4j.Logger;
@@ -9,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @Validated
@@ -29,6 +32,11 @@ public class PayloadController {
         LOGGER.debug(payloadRequest.getContent() + " " + payloadRequest.getTimestamp());
         messagePayloadService.pushMessageIntoQueue(payloadRequest.getMessagePayload());
         return new ResponseEntity<>(new GenericResponse<>(payloadRequest), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/message/all")
+    public List<EnrichedMessageDTO> getAllEnrichedMessages(){
+        return messagePayloadService.getAllMessages();
     }
 
 
