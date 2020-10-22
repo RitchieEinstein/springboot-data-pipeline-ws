3d4d8a0a908fa90c556a620cab3cab33ce904fe5
Ritchie Einstein <RitchieEinstein@users.noreply.github.com>
2020-10-22 14:19:27 +0530

    Uncommented all the maven build commands

--- initialize-and-start.sh
+++ initialize-and-start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#mvn -f eureka-naming-server/ clean package
-#mvn -f gateway-server/ clean package
+mvn -f eureka-naming-server/ clean package
+mvn -f gateway-server/ clean package
 mvn -f message-ingester/ clean package
-#mvn -f websock-streamer/ clean package
-#mvn -f db-persister/ clean package
+mvn -f websock-streamer/ clean package
+mvn -f db-persister/ clean package
 docker-compose up --build
